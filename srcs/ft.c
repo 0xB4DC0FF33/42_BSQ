8f91e5cf5c539969da7457e629089c24222fc5f7
Mathieu BURTE <mburte@e1r2p5.42.fr>
2015-08-27 00:10:24 +0200

    fin de la lecture du fichier, ajout des fonctions FT

--- srcs/ft.c
+++ srcs/ft.c
@@ -1,38 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mburte <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/08/25 14:25:06 by mburte            #+#    #+#             */
-/*   Updated: 2015/08/27 00:09:10 by mburte           ###   ########.fr       */
+/*   Created: 2015/08/27 00:05:16 by mburte            #+#    #+#             */
+/*   Updated: 2015/08/27 00:09:19 by mburte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
-{
-	char  *file;
-	int i;
+int ft_strlen(char *str)
 
-	i = 1;
-	if (argc == 1)
-	{
-		file = "stdinput";
-		io_open(file);
-	}
-	else if (argc == 2)
-	{
-		file = *argv[1];
-		io_open(file);
-	}
-	else if (argc > 2)
-	{
-		while (i <= argc)
-		{
-			file = *argv[i];
-			io_open(file);
-			i++;
-		}
-	}
 }
--- srcs/inputOutput.c
+++ srcs/inputOutput.c
@@ -6,7 +6,7 @@
 /*   By: mburte <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/25 16:27:01 by mburte            #+#    #+#             */
-/*   Updated: 2015/08/25 23:55:35 by mburte           ###   ########.fr       */
+/*   Updated: 2015/08/27 00:09:36 by mburte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,42 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
-char **IO_read(char *file)
+int io_open(char *file)
 {
-	char buff[BUFF_SIZE + 1];
 	int fd;
 
-	fd = open(file, O_RDONLY);
-	
+	if (file == "stdinput")
+		fd = 0;
+	else
+		fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr(errno);
+		return (0);
+	}
+	else
+	{
+		IO_read(fd);
+		return(fd);
+	}
+}
+
+char *io_read(int fd)
+{
+	char buf[BUFF_SIZE + 1];
+	char *str;
+
+	if (read(fd, buf, BUFF_SIZE) == -1)
+	{
+		ft_putstr(errno);
+		return(0);
+	}
+	else
+	{
+		str = read(fd, buf, BUFF_SIZE);
+	}
+	ct_strtotab(str);
+	return (str);
 }
